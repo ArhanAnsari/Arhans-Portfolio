bb518517c92df47dd47be0f89fde6819679ef208
Arhan Ansari <arhanansari2009@gmail.com>
2024-04-22 13:16:55 +0530

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -27,9 +27,9 @@ export const LoadingScreen = (props) => {
             width: `${progress}%`,
           }}
         >
-          Wawa Sensei
+          Arhan Ansari
         </div>
-        <div className="opacity-40">Wawa Sensei</div>
+        <div className="opacity-40">Arhan Ansari</div>
       </div>
     </div>
   );
