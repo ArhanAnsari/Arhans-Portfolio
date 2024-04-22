66d76d031de547dec9e9713502a4108528eaa4bf
Arhan Ansari <arhanansari2009@gmail.com>
2024-04-22 08:31:57 +0000

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,89 +7,123 @@ import { atom, useAtom } from "jotai";
 import { useEffect, useRef } from "react";
 
 export const projects = [
-  {
-    title: "Arhan Sales",
-    url: "arhansales.000webhostapp.com",
-    image: "projects/wawatmos.jpg",
-    description: "Website of my client",
-  },
+	{
+		title: "Arhan Sales",
+		url: "arhansales.000webhostapp.com",
+		image: "projects/arhansales.png",
+		description: "Website of my client",
+	},
+	{
+		title: "Car Game",
+		url: "https://arhanansari.github.io/carGameThreeJS/",
+		image: "projects/carGame.jpg",
+		description: "A car game made in ThreeJS",
+	},
 ];
 
 const Project = (props) => {
-  const { project, highlighted } = props;
+	const { project, highlighted } = props;
 
-  const background = useRef();
-  const bgOpacity = useMotionValue(0.4);
+	const background = useRef();
+	const bgOpacity = useMotionValue(0.4);
 
-  useEffect(() => {
-    animate(bgOpacity, highlighted ? 0.7 : 0.4);
-  }, [highlighted]);
+	useEffect(() => {
+		animate(bgOpacity, highlighted ? 0.7 : 0.4);
+	}, [highlighted]);
 
-  useFrame(() => {
-    background.current.material.opacity = bgOpacity.get();
-  });
+	useFrame(() => {
+		background.current.material.opacity = bgOpacity.get();
+	});
 
-  return (
-    <group {...props}>
-      <mesh
-        position-z={-0.001}
-        onClick={() => window.open(project.url, "_blank")}
-        ref={background}
-      >
-        <planeGeometry args={[2.2, 2]} />
-        <meshBasicMaterial color="black" transparent opacity={0.4} />
-      </mesh>
-      <Image
-        scale={[2, 1.2, 1]}
-        url={project.image}
-        toneMapped={false}
-        position-y={0.3}
-      />
-      <Text
-        maxWidth={2}
-        anchorX={"left"}
-        anchorY={"top"}
-        fontSize={0.2}
-        position={[-1, -0.4, 0]}
-      >
-        {project.title.toUpperCase()}
-      </Text>
-      <Text
-        maxWidth={2}
-        anchorX="left"
-        anchorY="top"
-        fontSize={0.1}
-        position={[-1, -0.6, 0]}
-      >
-        {project.description}
-      </Text>
-    </group>
-  );
+	return (
+		<group {...props}>
+			<mesh
+				position-z={-0.001}
+				onClick={() =>
+					window.open(project.url, "_blank")
+				}
+				ref={background}
+			>
+				<planeGeometry args={[2.2, 2]} />
+				<meshBasicMaterial
+					color="black"
+					transparent
+					opacity={0.4}
+				/>
+			</mesh>
+			<Image
+				scale={[2, 1.2, 1]}
+				url={project.image}
+				toneMapped={false}
+				position-y={0.3}
+			/>
+			<Text
+				maxWidth={2}
+				anchorX={"left"}
+				anchorY={"top"}
+				fontSize={0.2}
+				position={[-1, -0.4, 0]}
+			>
+				{project.title.toUpperCase()}
+			</Text>
+			<Text
+				maxWidth={2}
+				anchorX="left"
+				anchorY="top"
+				fontSize={0.1}
+				position={[-1, -0.6, 0]}
+			>
+				{project.description}
+			</Text>
+		</group>
+	);
 };
 
 export const currentProjectAtom = atom(Math.floor(projects.length / 2));
 
 export const Projects = () => {
-  const { viewport } = useThree();
-  const [currentProject] = useAtom(currentProjectAtom);
+	const { viewport } = useThree();
+	const [currentProject] = useAtom(currentProjectAtom);
 
-  return (
-    <group position-y={-viewport.height * 2 + 1}>
-      {projects.map((project, index) => (
-        <motion.group
-          key={"project_" + index}
-          position={[index * 2.5, 0, -3]}
-          animate={{
-            x: 0 + (index - currentProject) * 2.5,
-            y: currentProject === index ? 0 : -0.1,
-            z: currentProject === index ? -2 : -3,
-            rotateX: currentProject === index ? 0 : -Math.PI / 3,
-            rotateZ: currentProject === index ? 0 : -0.1 * Math.PI,
-          }}
-        >
-          <Project project={project} highlighted={index === currentProject} />
-        </motion.group>
-      ))}
-    </group>
-  );
+	return (
+		<group position-y={-viewport.height * 2 + 1}>
+			{projects.map((project, index) => (
+				<motion.group
+					key={"project_" + index}
+					position={[index * 2.5, 0, -3]}
+					animate={{
+						x:
+							0 +
+							(index -
+								currentProject) *
+								2.5,
+						y:
+							currentProject === index
+								? 0
+								: -0.1,
+						z:
+							currentProject === index
+								? -2
+								: -3,
+						rotateX:
+							currentProject === index
+								? 0
+								: -Math.PI / 3,
+						rotateZ:
+							currentProject === index
+								? 0
+								: -0.1 *
+									Math.PI,
+					}}
+				>
+					<Project
+						project={project}
+						highlighted={
+							index === currentProject
+						}
+					/>
+				</motion.group>
+			))}
+		</group>
+	);
 };
