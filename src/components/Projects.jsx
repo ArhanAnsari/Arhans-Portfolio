6b8c8d9e16034105dfe5fdcb9305961afe437138
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-08 15:15:57 +0530

    Update V3.3.3.2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -289,6 +289,12 @@ export const projects = [
 		image: "projects/quizapp.png",
 		description: "This project is created with HTML, CSS and JavaScript that allows user to answer given questions.It includes sections for starting the quiz, displaying quiz information. There’s a timer, for each question and its options. If the user doesn’t answer within the time limit, then the next question appears automatically. After selecting an option, it checks if it’s correct or not. At the end,it displays your score with a result message.It also has the option to restart the quiz or quit.",
 	},
+	{
+		title: "Paytm Clone",
+		url: "https://paytm-clone-brown-rho.vercel.app/",
+		image: "projects/paytmclone.png",
+		description: "This Paytm Home Page Clone project replicates the user interface and design of the Paytm company's homepage. Utilizing a combination of HTML, CSS, and Bootstrap, the project aims to recreate the visual aesthetics of the original Paytm homepage. Notably, the implementation is designed to be responsive, ensuring a seamless and consistent user experience across various devices and screen sizes.",
+	},
 ];
 
 const Project = (props) => {
