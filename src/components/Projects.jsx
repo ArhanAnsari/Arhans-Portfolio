7c0c72248f5a9b1d66f7d75e543ba10b56e80e3a
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-08 15:04:14 +0530

    Update V3.3.3.1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -283,6 +283,12 @@ export const projects = [
 		image: "projects/ArhanLaptop.in.jpg",
 		description: "A simple shopping website made with HTML,CSS,JS.",
 	},
+	{
+		title: "Quiz App",
+		url: "https://arhanansari.github.io/Quiz_App/",
+		image: "projects/quizapp.png",
+		description: "This project is created with HTML, CSS and JavaScript that allows user to answer given questions.It includes sections for starting the quiz, displaying quiz information. There’s a timer, for each question and its options. If the user doesn’t answer within the time limit, then the next question appears automatically. After selecting an option, it checks if it’s correct or not. At the end,it displays your score with a result message.It also has the option to restart the quiz or quit.",
+	},
 ];
 
 const Project = (props) => {
